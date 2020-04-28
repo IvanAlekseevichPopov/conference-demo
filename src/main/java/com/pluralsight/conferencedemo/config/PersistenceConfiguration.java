28f36059c3e72865246be45776d6f977597dd9eb
IvanAlekseevichPopov <ivan.alekseevich.popov@gmail.com>
2020-04-28 13:32:58 +0300

    commented hardcoded db url

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,15 +8,15 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/dev");
-        builder.username("dev");
-        builder.password("dev");
-        System.out.println("My custom datasource bean has been....");
-
-        return builder.build();
-    }
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/dev");
+//        builder.username("dev");
+//        builder.password("dev");
+//        System.out.println("My custom datasource bean has been....");
+//
+//        return builder.build();
+//    }
 }
